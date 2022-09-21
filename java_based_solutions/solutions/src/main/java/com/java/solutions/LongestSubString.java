9cc165b4c0b6fdad27fa487050b043f763214dcc
madhavan98 <77061054+madhavan98@users.noreply.github.com>
2022-09-22 00:52:59 +0530

    Added pom.xml

---
+++ java_based_solutions/solutions/src/main/java/com/java/solutions/LongestSubString.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.java.solutions;
+
+/**
+ * @author P K Madhavan
+ *
+ */
+public class LongestSubString {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
